89da3dca76f6a363b2d62f6d4e19c249c965fb14
mfatihaktas <mfatihaktas@gmail.com>
2017-02-17 10:33:28 -0500

    After getting good approximations for auto_repeat.

--- auto_repeat_models.py
+++ auto_repeat_models.py
@@ -2,6 +2,71 @@ import sys, pprint, random, math, numpy, simpy, getopt, itertools, sympy
 
 from patch import *
 
+# Pr{T >= t}
+def prob_T_k_n_geq_t(mu, d, k, n, t):
+  q = 1 - math.exp(-mu*d)
+  
+  # Pr{T >= t | T < d}*Pr{T < d}
+  def lhs():
+    if t > d:
+      return 0
+    q_ = 1 - math.exp(-mu*t)
+    # return q**k - q_**k
+    # return 1 - q_**k / q**k
+    return q**k - q_**k
+  # Pr{T >= t | T > d}*Pr{T > d}
+  def rhs():
+    # if t <= d:
+    #   return 0
+    def prob_X_n_r__k_r_leq_tau(r):
+      tau = max(0, t - d)
+      q_ = 1 - math.exp(-mu*tau)
+      # print("q_= {}".format(q_) )
+      sum_ = 0
+      for j in range(k-r, n-r+1):
+        sum_ += binomial(n-r,j) * q_**j * (1-q_)**(n-r-j)
+      return sum_
+    sum_ = 0
+    for r in range(k):
+      # print("prob_X_n_r__k_r_leq_tau(r= {})= {}".format(r, prob_X_n_r__k_r_leq_tau(r) ) )
+      sum_ += prob_X_n_r__k_r_leq_tau(r) * binomial(k,r) * q**r * (1-q)**(k-r)
+    return (1 - q**k - sum_) # / q**k
+  # print("lhs= {}, rhs= {}".format(lhs(), rhs() ) )
+  return lhs() + rhs()
+
+def prob_T_k_n_geq_t_alt(mu, d, k, n, t):
+  q = 1 - math.exp(-mu*d)
+  
+  # Pr{T >= t | T < d}*Pr{T < d}
+  def lhs():
+    if t > d:
+      return 0
+    q_ = 1 - math.exp(-mu*t)
+    # return q**k - q_**k
+    # return 1 - q_**k / q**k
+    return q**k - q_**k
+  # Pr{T >= t | T > d}*Pr{T > d}
+  def rhs():
+    # if t <= d:
+    #   return 0
+    def prob_X_n_r__k_r_leq_tau(r):
+      tau = max(0, t - d)
+      q_ = 1 - math.exp(-mu*tau)
+      
+      return sympy.mpmath.quad(lambda x: x**(k-r-1) * (1-x)**(n-k), [0, q_] ) / \
+             sympy.mpmath.quad(lambda x: x**(k-r-1) * (1-x)**(n-k), [0, 1] )
+    
+    # sum_ = 0
+    # for r in range(k):
+    #   # print("prob_X_n_r__k_r_leq_tau(r= {})= {}".format(r, prob_X_n_r__k_r_leq_tau(r) ) )
+    #   sum_ += prob_X_n_r__k_r_leq_tau(r) * binomial(k,r) * q**r * (1-q)**(k-r)
+    
+    # sum_ = prob_X_n_r__k_r_leq_tau((math.ceil(k*q) + math.floor(k*q) )/2) - prob_X_n_r__k_r_leq_tau(k)*q**k # math.ceil(k*q)
+    sum_ = prob_X_n_r__k_r_leq_tau(k*q) - prob_X_n_r__k_r_leq_tau(k)*q**k # math.ceil(k*q)
+    return (1 - q**k - sum_) # / q**k
+  # print("lhs= {}, rhs= {}".format(lhs(), rhs() ) )
+  return lhs() + rhs()
+
 def try_approx():
   mu = 1
   d = 2
@@ -16,15 +81,16 @@ def try_approx():
 # Send k initially, send n-k more after d
 def E_T_k_n(mu, d, k, n):
   p = math.exp(-mu*d)
-  q = 1-math.exp(-mu*d)
+  q = 1 - math.exp(-mu*d)
   
-  E_T = d*q**k - sympy.mpmath.quad(lambda x: (1-math.exp(-mu*x) )**k, [0, d] )
-  sum_ = 0 # d*(1 - q**k)
+  E_H_n_r = 0
   for r in range(k):
-     sum_ += (d + (H(n-r)-H(n-k) )/mu) * binomial(k, r)*(1-p)**r * p**(k-r)
-  E_T += sum_
+    E_H_n_r += ((H(n-r) )/mu) * binomial(k, r)*(1-p)**r * p**(k-r)
   
-  return E_T
+  print("d= {}, k= {}, n= {}\n\t E_H_n_r - H(n-k)= {}".format(d, k, n, E_H_n_r - H(n-k) ) )
+  
+  return d - sympy.mpmath.quad(lambda x: (1-math.exp(-mu*x) )**k, [0, d] ) + \
+         1/mu*(E_H_n_r - H(n-k) )
 
 # Is equal to E_T_k_n
 def E_T_k_n_alt(mu, d, k, n):
@@ -32,22 +98,25 @@ def E_T_k_n_alt(mu, d, k, n):
   q = 1-math.exp(-mu*d)
   E_H_n_r = 0
   for r in range(k+1):
-     E_H_n_r += H(n-r) * binomial(k, r) * q**r * (1-q)**(k-r)
-  
-  # return H(k)/mu * q**k + (d*(1 - q**k) + E_H_n_r/mu - H(n-k)/mu)*(1 - q**k)
-  # return d*q**k + \
-  # return d*q**k + -1/mu*(H(k) - (k*p + 1 - (1-p)**k)/2 - mu*d) + \
-  # return d*q**k + d - 1/mu*H(k) + \
-  # return d*q**k + q*H(k) + \
+    E_H_n_r += H(n-r) * binomial(k, r) * q**r * (1-q)**(k-r)
   
   # E_H_n_r = H(n-math.ceil(k*q) ) # H(n-math.floor(k*q) ) # H(n-math.ceil(k*q) ) # H(n-k) # (H(n) + H(n-k) )/2 # + 1/n*(1-q)**k
+  # E_H_n_r = math.log(n-k*q) + 0.5772156649 + 1/2/(n-k*q)
   
   # return d - sympy.mpmath.quad(lambda x: (1-math.exp(-mu*x) )**k, [0, d] ) + \
   # return d - sympy.mpmath.quad(lambda x: (1-math.exp(-mu*x) )**k, [0, d] ) + \
   # return d + -H(k) + \
   # return d + 1/mu*(H(k) - mu*d - sympy.mpmath.quad(lambda x: (1 - (1-x)**k)/x, [0, p] ) ) + \ # great
-  return d + sympy.mpmath.quad(lambda x: x**k * 1/(1-x), [0, q] ) + \
-         (E_H_n_r/mu - H(n-k)/mu)
+  
+  # return d - 1/mu*sympy.mpmath.quad(lambda x: x**k * 1/(1-x), [0, q] ) + \
+  #       (E_H_n_r/mu - H(n-k)/mu)
+  if n == k:
+    return d - 1/mu*sympy.mpmath.quad(lambda x: x**k * 1/(1-x), [0, q] )
+  
+  return d - 1/mu*sympy.mpmath.quad(lambda x: x**k * 1/(1-x), [0, q] ) + \
+         1/mu*(E_H_n_r - H(n-k) )
+        # 1/mu*(math.log((n-k*q)/(n-k) ) )
+        # 1/mu*(math.log((n-k*q)/(n-k) ) + 1/2/(n-k*q) - 1/2/(n-k) )
 
 # E[H(k-r)] where r ~ Binomial(k, p)
 def E_H_k_r(k, p):
--- deneme.py
+++ deneme.py
@@ -7,36 +7,98 @@ from cycler import cycler
 from auto_repeat_models import *
 
 # ------------------------  Checks on Pei's work  --------------------- #
-def plot_autorepeat_k_n():
+def plot_autorepeat_conf_k_n():
   mu = 1
   k = 10
-  n = 20
-  # d = 5
+  n = 20 # 15
+  # d = H(k)/mu * 0.3
+  L = 2
   
-  d_l, E_T_k_n_l, E_T_k_n_alt_l, E_T_k_n_lb_l = [], [], [], []
-  E_T_k_n_r_1_5_l, E_T_k_n_r_1_l = [], []
-  for d in numpy.arange(0.1, 3*H(k)/mu, 0.1):
+  d_l, prob_T_k_n_geq_L_l, prob_T_k_n_geq_L_alt_l = [], [], []
+  # E_T_k_n_11_l, E_T_k_n_r_1_5_alt_l, E_T_k_n_r_1_l, E_T_k_n_r_1_alt_l = [], [], [], []
+  # for d in numpy.arange(0.1, 3*H(k)/mu, 0.1):
+  for d in numpy.arange(0.1, L, 0.1):
     d_l.append(d)
     
-    E_T_k_n_l.append(E_T_k_n(mu, d, k, n) )
-    E_T_k_n_alt_l.append(E_T_k_n_alt(mu, d, k, n) )
-    E_T_k_n_r_1_5_l.append(E_T_k_n(mu, d, k, int(k*1.5) ) )
-    E_T_k_n_r_1_l.append(E_T_k_n(mu, d, k, k) )
-    
-    E_T_k_n_lb_l.append(E_T_k_n_lb(mu, d, k, n) )
-  
+    prob_T_k_n_geq_L_l.append(prob_T_k_n_geq_t(mu, d, k, n, L) )
+    prob_T_k_n_geq_L_alt_l.append(prob_T_k_n_geq_t_alt(mu, d, k, n, L) )
   marker = itertools.cycle(('^', 'p', 'x', '+', '*', 'v', 'o') )
-  plot.plot(d_l, E_T_k_n_l, color='black', label=r'$E[T]$', marker=next(marker), linestyle='', mew=2)
-  plot.plot(d_l, E_T_k_n_alt_l, color='brown', label=r'$E[T_{alt}]$', marker=next(marker), linestyle='', mew=2)
-  # plot.plot(d_l, E_T_k_n_r_1_5_l, color='magenta', label=r'$E[T]$, r:1.5', marker=next(marker), linestyle='', mew=2)
+  plot.plot(d_l, prob_T_k_n_geq_L_l, color='black', label=r'$Pr\{T \geq L\}$', marker=next(marker), linestyle='', mew=2)
+  plot.plot(d_l, prob_T_k_n_geq_L_alt_l, color='brown', label=r'$Pr\{T_{alt} \geq L\}$', marker=next(marker), linestyle='', mew=2)
+  plot.legend()
+  plot.xlabel(r'$d$')
+  plot.ylabel(r'$Pr\{T \geq L\}$')
+  plot.title(r'mu= {}, k= {}, n= {}, L= {}'.format(mu, k, n, L) )
+  plot.savefig("plot_autorepeat_conf_k_{}_n_{}.png".format(k, n) )
+  log(WARNING, "done; k= {}, n= {}".format(k, n) )
+
+def plot_autorepeat_dist_k_n():
+  mu = 1
+  k = 10
+  n = 10
+  d = H(k)/mu * 0.3
+  
+  t_l, prob_T_k_n_geq_t_l, prob_T_k_n_geq_t_alt_l = [], [], []
+  # E_T_k_n_11_l, E_T_k_n_r_1_5_alt_l, E_T_k_n_r_1_l, E_T_k_n_r_1_alt_l = [], [], [], []
+  # for d in numpy.arange(0.1, 3*H(k)/mu, 0.1):
+  for t in numpy.arange(0, 3*d, 0.1):
+    t_l.append(t)
+    
+    prob_T_k_n_geq_t_l.append(prob_T_k_n_geq_t(mu, d, k, n, t) )
+    prob_T_k_n_geq_t_alt_l.append(prob_T_k_n_geq_t_alt(mu, d, k, n, t) )
+    # E_T_k_n_11_l.append(E_T_k_n(mu, d, k, int(k*1.5) ) )
+    # E_T_k_n_r_1_5_alt_l.append(E_T_k_n_alt(mu, d, k, int(k*1.5) ) )
+    # E_T_k_n_r_1_l.append(E_T_k_n(mu, d, k, k) )
+    # E_T_k_n_r_1_alt_l.append(E_T_k_n_alt(mu, d, k, k) )
+    # E_T_k_n_lb_l.append(E_T_k_n_lb(mu, d, k, n) )
+  marker = itertools.cycle(('^', 'p', 'x', '+', '*', 'v', 'o') )
+  plot.plot(t_l, prob_T_k_n_geq_t_l, color='black', label=r'$Pr\{T \geq t\}$', marker=next(marker), linestyle='', mew=2)
+  plot.plot(t_l, prob_T_k_n_geq_t_alt_l, color='brown', label=r'$Pr\{T_{alt} \geq t\}$', marker=next(marker), linestyle='', mew=2)
+  # plot.plot(d_l, E_T_k_n_11_l, color='magenta', label=r'$E[T]$, r:1.5', marker=next(marker), linestyle='', mew=2)
+  # plot.plot(d_l, E_T_k_n_r_1_5_alt_l, color='pink', label=r'$E[T_{alt}]$, r:1.5', marker=next(marker), linestyle='', mew=2)
   # plot.plot(d_l, E_T_k_n_r_1_l, color='red', label=r'$E[T]$, r:1', marker=next(marker), linestyle='', mew=2)
   # plot.plot(d_l, E_T_k_n_lb_l, color='green', label=r'$E[\hat{T}_{LB}]$', marker=next(marker), linestyle='', mew=2)
   plot.legend()
+  plot.xlabel(r'$t$')
+  plot.ylabel(r'$Pr\{T \geq t\}$')
+  plot.title(r'mu= {}, k= {}, n= {}, d= {}'.format(mu, k, n, d) )
+  plot.savefig("plot_autorepeat_dist_k_{}_n_{}.png".format(k, n) )
+  log(WARNING, "done; k= {}, n= {}".format(k, n) )
+  
+def plot_autorepeat_k_n():
+  mu = 1
+  k = 10
+  N = 20
+  
+  d_l = []
+  n__E_T_l_m, n__E_T_model_l_m = {}, {}
+  
+  
+  first_loop = True
+  k_step = 1
+  for n in range(k, N, k_step):
+    n__E_T_l_m[n] = []
+    n__E_T_model_l_m[n] = []
+    for d in numpy.arange(0.1, 2*H(k)/mu, 0.1):
+      if first_loop:
+        d_l.append(d)
+      n__E_T_l_m[n].append(E_T_k_n(mu, d, k, n) )
+      # n__E_T_model_l_m[n].append(E_T_k_n_alt(mu, d, k, n) )
+    first_loop = False
+  marker = itertools.cycle(('^', 'p', 'x', '+', '*', 'v', 'o') )  
+  color = iter(cm.rainbow(numpy.linspace(0, 1, int((N-k)/k_step) ) ) )
+  plot.plot(d_l, d_l, label=r'$y=d$', color="black", marker='.', linestyle='', mew=2)
+  for n in range(k, N, k_step):
+    m = next(marker)
+    c = next(color)
+    plot.plot(d_l, n__E_T_l_m[n], label=r'$E[T]$, n:{}'.format(n), color=c, marker=m, linestyle='', mew=2)
+    # plot.plot(d_l, n__E_T_model_l_m[n], label=r'$E[\hat{T}]$', color=c, alpha=0.7, marker=m, linestyle='', mew=2)
+  plot.legend()
   plot.xlabel(r'$d$')
   plot.ylabel("E[T] (s)")
-  plot.title(r'mu= {}, k= {}, n= {}'.format(mu, k, n) )
-  plot.savefig("plot_autorepeat_k_{}_n_{}.png".format(k, n) )
-  log(WARNING, "done; k= {}, n= {}".format(k, n) )
+  plot.title(r'$\mu$= {}, k= {}'.format(mu, k) )
+  plot.savefig("plot_autorepeat_k_{}.png".format(k) )
+  log(WARNING, "done; k= {}".format(k) )
 
 def plot_prob_N_d():
   k = 10
@@ -121,3 +183,5 @@ if __name__ == "__main__":
   # plot_den()
   # plot_binomial_dist__approx()
   plot_autorepeat_k_n()
+  # plot_autorepeat_dist_k_n()
+  # plot_autorepeat_conf_k_n()
